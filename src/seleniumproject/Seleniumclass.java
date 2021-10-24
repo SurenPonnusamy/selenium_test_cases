74c273fc6f7191d11ac944e5cc867275ced7e1b0
Ponnusamy <surendharselvakumar@gmail.com>
2021-10-24 13:17:46 +0530

    Changes with 3 test cases and priority

--- src/seleniumproject/Seleniumclass.java
+++ src/seleniumproject/Seleniumclass.java
@@ -6,35 +6,44 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
-import seleniumproject.driverClass.*;
+import seleniumproject.Driverclass.*;
 
-@Test
-public class Seleniumclass {
+
+public class Seleniumclass extends Driverclass {	
 	
-	WebDriver driver;
-	
+	@Test(priority = 1)
 	public void loginPageTest() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\surendhar.ponnusamy\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("http://localhost:81/");
+		driver.get("http://localhost:81");
 		driver.findElement(By.id("formBasicEmail")).sendKeys("admin"); 
 		driver.findElement(By.id("formBasicPassword")).sendKeys("admin");
 		driver.findElement(By.id("submitBtn")).click();
 		String title = driver.findElement(By.id("homeHeading")).getText();
 		String expectedString = "CNAP sample react CICD dashboard";
+		System.out.println(title);
 		assertEquals(title, expectedString);
-		driver.close();
 	}
+	
+	@Test(priority = 2)
 	public void addBuildServerTest() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\surendhar.ponnusamy\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://localhost:81/home");
 		driver.findElement(By.id("buildServerIcon")).click();
 		String buildServertitle = driver.findElement(By.id("buildServerHeading")).getText();
+		System.out.println(buildServertitle);
 		String expectedBuildServertitle = "CNAP Build Server";
 		assertEquals(buildServertitle, expectedBuildServertitle);
+	}
+	
+	@Test(priority = 3)
+	public void addPipelineTest() {
+		driver.manage().window().maximize();
+		driver.get("http://localhost:81/home");
+		driver.findElement(By.id("pipelineConfIcon")).click();
+		String pipelineConf = driver.findElement(By.id("pipelineConfiguration")).getText();
+		System.out.println(pipelineConf);
+		String expectedPipelineConf = "CNAP Pipeline Configuration";
+		assertEquals(pipelineConf, expectedPipelineConf);
 		driver.close();
 	}
 }
--- src/seleniumproject/driverClass.java
+++ src/seleniumproject/driverClass.java
@@ -1,15 +1,18 @@
 package seleniumproject;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 
-
-public class driverClass {
-
-	WebDriver driver;
+public class Driverclass {
 	
-	public void setDriver() {
+	public static WebDriver driver;
+	
+
+	@BeforeClass
+	public static void launchApplication() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\surendhar.ponnusamy\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		
+		driver = new ChromeDriver();
 	}
-	
+
 }
